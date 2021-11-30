79a735cce6c307e7b1e6e6e8e4b6a923ee943d79
Brendon <brendon.seaton@gmail.com>
2021-11-29 19:36:06 -0500

    Board printing functionality added

---
+++ board.py
@@ -0,0 +1,13 @@
+def print_board():
+
+    board_top = ['-', '-', '-']
+    board_mid = ['-', '-', '-']
+    board_bot = ['-', '-', '-']
+    board = [board_top, board_mid, board_bot]
+
+    print("Printing board...")
+    for i in board:
+        print(i)
+
+
+print_board()
